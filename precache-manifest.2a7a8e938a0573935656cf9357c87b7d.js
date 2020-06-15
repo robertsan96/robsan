d830af9a5811c775e5a67580e9d773e65b6027b8
robertsan96 <robertsandru96@gmail.com>
2020-06-15 07:53:11 +0300

    Updates

---
+++ precache-manifest.2a7a8e938a0573935656cf9357c87b7d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c83253c3bd6603281f1594475342ef2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af7f00a8594564d5b838",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "016b373e21c56a89ba9c",
+    "url": "/static/js/2.f687250c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f687250c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af7f00a8594564d5b838",
+    "url": "/static/js/main.095ae491.chunk.js"
+  },
+  {
+    "revision": "a537452989322ad1f64e",
+    "url": "/static/js/runtime-main.40f6f44f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
